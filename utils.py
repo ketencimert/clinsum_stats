87008ed37f2ab7cfa57c64a4551c2a2f504abb5c
Mert Ketenci <mk4139@columbia.edu>
2020-06-28 22:14:57 +0300

    initial commit

--- utils.py
+++ utils.py
@@ -4,9 +4,9 @@ Created on Sun Jun 28 13:10:49 2020
 
 @author: Mert Ketenci
 """
+import re
 
 def remove_html_tags(text):
 
-    import re
     clean = re.compile('<.*?>')
     return re.sub(clean, '', text)
